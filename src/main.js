c32598e5bc0c9fc9f29de36e29f393f53eb34573
Jannik Krämer <kraemer.jannik@gmail.com>
2018-03-07 17:34:33 +0100

    Added groups

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Bridge from './core/bridge';
 import Lights from './core/lights';
+import Groups from './core/groups';
 
 import * as informations from './const/info';
 
 export const bridge = new Bridge();
 export const lights = new Lights();
+export const groups = new Groups();
 
 export const version = () => {
     return informations.version;
