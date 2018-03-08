2433ec268046ffa6c64f61618dff0ec804302abb
Jannik Krämer <kraemer.jannik@gmail.com>
2018-03-08 16:33:36 +0100

    Updated constants

---
+++ src/const/error.js
@@ -0,0 +1,11 @@
+export const lightNotFound = (light) => {
+    return "Light (" + light + ") not found!";
+};
+
+export const groupNotFound = (group) => {
+    return "Group (" + group + ") not found!"; 
+};
+
+export const bridgeNotFound = (bridgeIP) => {
+    return "Bridge with IP (" + bridgeIP + ") not found!";
+};
