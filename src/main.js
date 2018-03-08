1d5962430ac13e317ec9a4048d377334f898c41a
Jannik Krämer <kraemer.jannik@gmail.com>
2018-03-08 16:34:06 +0100

    Updated main file

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 import Bridge from './core/bridge';
 import Lights from './core/lights';
 import Groups from './core/groups';
-
-import * as informations from './const/info';
+import * as config from '../package.json';
 
 export const bridge = new Bridge();
 export const lights = new Lights();
 export const groups = new Groups();
 
+
+export { group } from './core/group';
+export { light } from './core/light';
+
 export const version = () => {
-    return informations.version;
+    return config.version;
 }
